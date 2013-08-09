25398333070b65e1b0d5542e5df77e80c4369aac
Chirag Anand <anand.chirag@gmail.com>
2013-08-09 12:35:27 +0000

    Modified market residuals test case because of new API

--- pkg/inst/tests/test_marketresiduals.R
+++ pkg/inst/tests/test_marketresiduals.R
@@ -4,10 +4,12 @@ test_that("test.market.residuals", {
 library(eventstudies)
 
 load(system.file("data", "StockPriceReturns.rda", package = "eventstudies"))
+load(system.file("data", "nifty.index.rda", package = "eventstudies"))
 
-mm.result <- marketResidual(data.object=StockPriceReturns[,c("BHEL","nifty")],
-                            market.name="nifty")
-mm.result <- mm.result[complete.cases(mm.result),]
+mm.result <- marketResidual(firm.returns=StockPriceReturns[,c("BHEL")],
+                            market.returns=nifty.index)
+mm.result <- xts(mm.result[complete.cases(mm.result),])
+colnames(mm.result) <- "BHEL"
 
 # Calculating manually
 result <- lm(BHEL ~ nifty, data=StockPriceReturns)

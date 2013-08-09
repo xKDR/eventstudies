8a2655cb3549f69475d6b0959dca07cfc1f9e3d1
Chirag Anand <anand.chirag@gmail.com>
2013-08-09 12:54:27 +0000

    Fixed test cases and examples according to the new variables for market returns and firm returns.

--- pkg/inst/tests/test_marketresiduals.R
+++ pkg/inst/tests/test_marketresiduals.R
@@ -6,13 +6,17 @@ library(eventstudies)
 load(system.file("data", "StockPriceReturns.rda", package = "eventstudies"))
 load(system.file("data", "nifty.index.rda", package = "eventstudies"))
 
+alldata <- merge(StockPriceReturns, nifty.index, all = TRUE)
+StockPriceReturns <- alldata[,-which(colnames(alldata) %in% "nifty.index")]
+nifty.index <- alldata$nifty.index
+
 mm.result <- marketResidual(firm.returns=StockPriceReturns[,c("BHEL")],
                             market.returns=nifty.index)
 mm.result <- xts(mm.result[complete.cases(mm.result),])
 colnames(mm.result) <- "BHEL"
 
 # Calculating manually
-result <- lm(BHEL ~ nifty, data=StockPriceReturns)
+result <- lm(BHEL ~ nifty.index, data=StockPriceReturns)
 resid.res <- xts(result$resid,as.Date(attr(result$resid,"names")))
 colnames(resid.res) <- "BHEL"
 

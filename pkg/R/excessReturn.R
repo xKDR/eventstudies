422854d98e53618dc27b7e8fd23173bd3b555c9d
Chirag Anand <anand.chirag@gmail.com>
2014-03-16 10:23:18 +0000

    Added object classes, modified description, added FIXME to the code.

--- pkg/R/excessReturn.R
+++ pkg/R/excessReturn.R
@@ -2,6 +2,7 @@
 # Excess return
 ###############
 # Argument:
+## FIXME: data.object??
 # 1. data.object: This is a time series object with firm return and market return
 # 2. market.name: It is the market (index) column name in the data object
 # Output:
@@ -9,6 +10,6 @@
 
 excessReturn <- function(firm.returns, market.returns){
   ## Getting market return
-  ma.ret <- firm.returns-market.returns
+  ma.ret <- firm.returns - market.returns
   return(ma.ret)
 }

81b98ad4c0bd00d210e846e0df58398110dc5b4e
Chirag Anand <anand.chirag@gmail.com>
2014-04-01 12:03:59 +0000

    Renamed nifty.index data.

--- pkg/inst/tests/test_marketresiduals.R
+++ pkg/inst/tests/test_marketresiduals.R
@@ -4,19 +4,19 @@ test_that("test.market.residuals", {
 library(eventstudies)
 
 load(system.file("data", "StockPriceReturns.rda", package = "eventstudies"))
-load(system.file("data", "nifty.index.rda", package = "eventstudies"))
+load(system.file("data", "NiftyIndex.rda", package = "eventstudies"))
 
-alldata <- merge(StockPriceReturns, nifty.index, all = TRUE)
-StockPriceReturns <- alldata[,-which(colnames(alldata) %in% "nifty.index")]
-nifty.index <- alldata$nifty.index
+alldata <- merge(StockPriceReturns, NiftyIndex, all = TRUE)
+StockPriceReturns <- alldata[,-which(colnames(alldata) %in% "NiftyIndex")]
+NiftyIndex <- alldata$NiftyIndex
 
 mm.result <- marketResidual(firm.returns=StockPriceReturns[,c("BHEL")],
-                            market.returns=nifty.index)
+                            market.returns=NiftyIndex)
 mm.result <- xts(mm.result[complete.cases(mm.result),])
 colnames(mm.result) <- "BHEL"
 
 # Calculating manually
-result <- lm(BHEL ~ nifty.index, data=StockPriceReturns)
+result <- lm(BHEL ~ NiftyIndex, data=StockPriceReturns)
 resid.res <- xts(result$resid,as.Date(attr(result$resid,"names")))
 colnames(resid.res) <- "BHEL"
 

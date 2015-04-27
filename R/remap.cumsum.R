0c834d4597d87909af3b603a160d5c92023cd5f2
Sargam Jain <sargamjain13@gmail.com>
2015-04-27 13:36:39 +0530

    Removed the bug for remap.cumsum and wilcox sign test.

--- R/remap.cumsum.R
+++ R/remap.cumsum.R
@@ -1,16 +1,16 @@
 
 # If is.pc then a value like "1" means 0.01
-remap.cumsum <- function(z, is.pc=FALSE, base=0) {
+remap.cumsum <- function(z, is.pc = FALSE, base = 0) {
   z <- firstValueZero(z)
   for (i in 1:NCOL(z)) {
     tmp <- z[,i]
     if (is.pc) {
       tmp <- tmp/100
     }
-    if(NCOL(z)==1){
-      z <- base+cumsum(tmp)
+    if(NCOL(z) == 1){
+      z <- base + cumsum(tmp)
     } else {
-      z[,i] <- base+cumsum(tmp)
+      z[,i] <- base + cumsum(tmp)
     }
   }
   z

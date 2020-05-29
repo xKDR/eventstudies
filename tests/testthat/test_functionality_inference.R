71836d2ae7114e8d8c92319c34068473445b1751
Chirag Anand <anand.chirag@gmail.com>
2020-05-29 20:13:24 +0530

    Add test case, fix existing

--- tests/testthat/test_functionality_inference.R
+++ tests/testthat/test_functionality_inference.R
@@ -52,25 +52,39 @@ test_that("functionality for inference functions", {
                                         # Classic strategy
   test.classic <- inference.classic(es.w = test.eventtime0,
                                     to.plot = FALSE)
-  testing.inference <- function(strategy){
 
-      cat("\nTesting for class of the object returned")
-
+  testing.inference <- function(strategy, result) {
+      message("Testing for class of the object returned")
+      expect_that(strategy, is_a("matrix"))
       expect_that(length(which(colnames(strategy) %in% c("2.5%",
                                                          "Mean",
                                                          "Median",
                                                          "97.5%"))),
                   equals(3))
 
-      cat("\nTestimg for number of data points returned in the object")
+      message("Testing for number of data points returned in the object")
       expect_that(nrow(strategy), equals(nrow(test.eventtime0)))
-      
-      cat("\nTesting for mean estimates and confidence intervals")
-      expect_that(class(strategy), equals("matrix"))
+            
+      message("Testing for mean estimates and confidence intervals")
+      expect_that(strategy, equals(result))
   }
 
-  testing.inference(strategy = test.boot)
-  testing.inference(strategy = test.wilcox)
-  testing.inference(strategy = test.classic)
-  
+  x <- structure(c(0, -0.0153232381366424, -0.0408950663236913, 0, 0.00984612009294346, 
+                   0.00699588521385023, 0, 0.0278562751459042, 0.0644716084647099),
+                 .Dim = c(3L, 3L),
+                 .Dimnames = list(NULL, c("2.5%", "Mean", "97.5%")))
+  testing.inference(strategy = test.boot, result = x)
+
+  x <- structure(c(0, -0.0153232381366424, -0.0408950663236913, 0, 0.0170053232695686, 
+                   -0.00258888649946787, 0, 0.0278562751459042, 0.0644716084647099),
+                 .Dim = c(3L, 3L),
+                 .Dimnames = list(NULL, c("2.5%", "Median", "97.5%")))
+  testing.inference(strategy = test.wilcox, result = x)
+
+  x <- structure(c(NaN, -0.0459535096314264, -0.125491237730001, 0, 
+                   0.00984612009294346, 0.00699588521385023, NaN, 0.0656457498173133, 
+                   0.139483008157702),
+                 .Dim = c(3L, 3L),
+                 .Dimnames = list(NULL, c("2.5%", "Mean", "97.5%")))
+  testing.inference(strategy = test.classic, result = x)
 })

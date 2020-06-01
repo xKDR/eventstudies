32bba50c2ee586e7e4b0460f3e5df0ad1b28b752
Chirag Anand <anand.chirag@gmail.com>
2020-06-01 14:03:58 +0530

    whitespace cleanup

--- tests/testthat/test_userinput_remap.R
+++ tests/testthat/test_userinput_remap.R
@@ -76,14 +76,14 @@ test_that("userinput for remap.functions", {
   es.test.remap3 <- remap.cumprod(es.test.w1, is.pc = FALSE,
                                   base = 1)
 
-  x <- structure(c(1, 0.991160637575446, 0.978001732201615, 0.991154528460188, 
+  x <- structure(c(1, 0.991160637575446, 0.978001732201615, 0.991154528460188,
                    1.00800943162675, 0.988258283374288, 0.975538547303955,
                    1.00273566114767, 1.01899110383749),
                  index = -4:4,
                  class = "zoo")
   expect_equal(x, es.test.remap2)
 
-  y  <- structure(c(1, 1.0278562751459, 1.0654915752642, 1, 1.01700532326957, 
+  y  <- structure(c(1, 1.0278562751459, 1.0654915752642, 1, 1.01700532326957,
                     0.997077907629198, 1, 0.984676761863358, 0.959496776889208),
                   .Dim = c(3L, 3L),
                   .Dimnames = list(NULL, c("1", "2", "3")),
@@ -92,38 +92,38 @@ test_that("userinput for remap.functions", {
   expect_equal(y, es.test.remap3)
 
   ### Testing remap.cumsum for percentage and absolute returns
-  
+
   cat("\nTesting for percentage and absolute returns in remap.cumsum")
-  
+
   es.test.remap4 <- remap.cumsum(es.test.w0, is.pc = TRUE, base = 0)
-  
+
   es.test.remap5 <- remap.cumsum(es.test.w0, is.pc = FALSE, base = 0)
 
-  x <- structure(c(0, -0.0000883936242455352, -0.000221156214636737, 
-                   -0.0000866697835241492, 0.0000833834491715368, -0.000112558648518828, 
+  x <- structure(c(0, -0.0000883936242455352, -0.000221156214636737,
+                   -0.0000866697835241492, 0.0000833834491715368, -0.000112558648518828,
                    -0.000241267269375545, 0.0000375234961855053, 0.00019963444246633
                    ), index = -4:4, class = "zoo")
   expect_equal(x, es.test.remap4)
 
-  y <- structure(c(0, -0.00883936242455352, -0.0221156214636737, -0.00866697835241492, 
-                   0.00833834491715368, -0.0112558648518828, -0.0241267269375545, 
+  y <- structure(c(0, -0.00883936242455352, -0.0221156214636737, -0.00866697835241492,
+                   0.00833834491715368, -0.0112558648518828, -0.0241267269375545,
                    0.00375234961855053, 0.019963444246633), index = -4:4, class = "zoo")
   expect_equal(y, es.test.remap5)
 
   ### Testing remap.cumprod for percentage and absolute returns
-  
+
   cat("\nTesting for percentage and absolute returns in remap.cumprod")
-  
+
   es.test.remap6 <- remap.cumsum(es.test.w0, is.pc = TRUE, base = 100)
-  
+
   es.test.remap7 <- remap.cumsum(es.test.w0, is.pc = FALSE, base = 100)
 
-  x <- structure(c(100, 99.9999116063758, 99.9997788437854, 99.9999133302165, 
+  x <- structure(c(100, 99.9999116063758, 99.9997788437854, 99.9999133302165,
                    100.000083383449, 99.9998874413515, 99.9997587327306,
                    100.000037523496, 100.000199634442), index = -4:4, class = "zoo")
   expect_equal(x, es.test.remap6)
 
-  y <- structure(c(100, 99.9911606375754, 99.9778843785363, 99.9913330216476, 
+  y <- structure(c(100, 99.9911606375754, 99.9778843785363, 99.9913330216476,
                    100.008338344917, 99.9887441351481, 99.9758732730624,
                    100.003752349619, 100.019963444247), index = -4:4, class = "zoo")
   expect_equal(y, es.test.remap7)
